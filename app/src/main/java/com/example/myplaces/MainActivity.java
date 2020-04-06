9f8de277ec155483c41655b19480b851a64c8515
abstract94 <mth471994@gmail.com>
2020-04-06 22:32:05 +0200

    third commit

--- app/src/main/java/com/example/myplaces/MainActivity.java
+++ app/src/main/java/com/example/myplaces/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //first commit
+    //first
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
